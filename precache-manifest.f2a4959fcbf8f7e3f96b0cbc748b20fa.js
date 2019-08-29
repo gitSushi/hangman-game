68b98958d549c824a0a6de8d74d2398b0305ff2d
gitSushi <lossushi37@gmail.com>
2019-08-29 17:26:14 +0200

    Updates

---
+++ precache-manifest.f2a4959fcbf8f7e3f96b0cbc748b20fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad060f9b0c21ae48d4d635f1c1179cf8",
+    "url": "/hangman-game/index.html"
+  },
+  {
+    "revision": "d0532bb296da2f49489c",
+    "url": "/hangman-game/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "5fbc8e379504123b3e47",
+    "url": "/hangman-game/static/css/main.b459fb1c.chunk.css"
+  },
+  {
+    "revision": "d0532bb296da2f49489c",
+    "url": "/hangman-game/static/js/2.32cfea5b.chunk.js"
+  },
+  {
+    "revision": "5fbc8e379504123b3e47",
+    "url": "/hangman-game/static/js/main.3ce91bd7.chunk.js"
+  },
+  {
+    "revision": "c49ca3397e61ef35670c",
+    "url": "/hangman-game/static/js/runtime~main.41ae35bd.js"
+  }
+]);
